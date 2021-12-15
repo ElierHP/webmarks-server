eb34367872947d5f36cbae7483756de166661302
Elier H <elierporto@gmail.com>
2021-12-15 18:19:09 -0500

    Feature: folder new, delete & edit routes

--- controllers/folder.js
+++ controllers/folder.js
@@ -1,3 +1,43 @@
-module.exports.findFolders = (req, res) => {
-  res.send("Folder Route");
+const Folder = require("../models/folder");
+
+module.exports.find = async (req, res) => {
+  const folders = await Folder.find({});
+  res.send(folders);
+};
+
+module.exports.new = async (req, res, next) => {
+  try {
+    const { title, parent_id } = req.body;
+    const folder = await new Folder({
+      title,
+      parent_id,
+    });
+    folder.save();
+    res.send(folder);
+  } catch (error) {
+    console.log("Error creating new folder.");
+    return next(error);
+  }
+};
+
+module.exports.delete = async (req, res, next) => {
+  try {
+    const { _id } = req.body;
+    const folder = await Folder.findByIdAndDelete(_id);
+    res.send(folder);
+  } catch (error) {
+    console.log("Error deleting folder.");
+    return next(error);
+  }
+};
+
+module.exports.edit = async (req, res, next) => {
+  try {
+    const { _id, title } = req.body;
+    const folder = await Folder.findByIdAndUpdate(_id, { title });
+    res.send(folder);
+  } catch (error) {
+    console.log("Error editing folder.");
+    return next(error);
+  }
 };
--- routes/folder.js
+++ routes/folder.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const folders = require("../controllers/folder");
 
-router.get("/", folders.findFolders);
+router.get("/", folders.find);
+
+router.post("/new", folders.new);
+
+router.patch("/edit", folders.edit);
+
+router.delete("/delete", folders.delete);
 
 module.exports = router;
