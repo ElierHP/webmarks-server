002b2aaab97a9f583bb50d2b2129e28f3980fc7d
Elier H <elierporto@gmail.com>
2022-01-03 17:14:16 -0500

    Feature: User routes

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,3 +1,32 @@
-module.exports.userController = (req, res) => {
-  res.send("User Route");
+const User = require("../models/user");
+
+module.exports.userData = async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+};
+
+module.exports.newUser = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = new User({ username: username });
+  User.register(user, password, (err) => {
+    if (err) {
+      console.log("error registering user!", err);
+      return next(err);
+    }
+  });
+  req.login(user, function (err) {
+    if (err) {
+      return next(err);
+    }
+  });
+  await res.send(req.user);
+};
+
+module.exports.login = (req, res) => {
+  res.send(req.body.username);
+};
+
+module.exports.logout = (req, res) => {
+  req.logout();
+  res.send("user has been logged out.");
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const users = require("../controllers/user");
+const passport = require("passport");
 
-router.get("/", users.userController);
+router.route("/").get(users.userData);
+
+router.post("/new", users.newUser);
+
+router.post("/login", passport.authenticate("local"), users.login);
+
+router.post("/logout", users.logout);
 
 module.exports = router;
