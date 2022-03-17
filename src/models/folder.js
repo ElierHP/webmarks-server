c0d0f57f17499533eb9a8af3a2d9235500611d69
Elier H <elierporto@gmail.com>
2022-03-17 13:52:40 -0400

    Add: timestamps to all models

--- src/models/folder.js
+++ src/models/folder.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const folderSchema = Schema({
-  type: {
-    type: String,
-    required: true,
+const folderSchema = Schema(
+  {
+    type: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    parent_id: {
+      type: String,
+      required: true,
+    },
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  parent_id: {
-    type: String,
-    required: true,
-  },
-  user_id: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Folder", folderSchema);
--- src/models/link.js
+++ src/models/link.js
@@ -1,27 +1,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const linkSchema = Schema({
-  type: {
-    type: String,
-    required: true,
+const linkSchema = Schema(
+  {
+    type: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    parent_id: {
+      type: String,
+      required: true,
+    },
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  url: {
-    type: String,
-    required: true,
-  },
-  parent_id: {
-    type: String,
-    required: true,
-  },
-  user_id: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Link", linkSchema);
